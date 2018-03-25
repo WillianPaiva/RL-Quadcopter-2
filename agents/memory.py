81c8da4bd1125da9a7ff6585ba447612aa2cd057
WillianPaiva <wpaiva@lucine.io>
2018-03-25 17:37:27 +0200

    temp

---
+++ agents/memory.py
@@ -0,0 +1,19 @@
+from collections import deque
+import numpy as np
+
+class Memory():
+    def __init__(self, max_size=1000):
+        self.buffer = deque(maxlen=max_size)
+
+    def add(self, experience):
+        self.buffer.append(experience)
+
+    def sample(self, batch_size):
+        idx = np.random.choice(np.arange(len(self.buffer)),
+                              size=batch_size,
+                               replace=False)
+        return [self.buffer[ii] for ii in idx]
+
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return len(self.buffer)
--- task.py
+++ task.py
@@ -1,34 +1,31 @@
 import numpy as np
 from physics_sim import PhysicsSim
+import random
 
 class Task():
     """Task (environment) that defines the goal and provides feedback to the agent."""
-    def __init__(self, init_pose=None, init_velocities=None, 
-        init_angle_velocities=None, runtime=5., target_pos=None):
+    def __init__(self, ):
         """Initialize a Task object.
         Params
         ======
-            init_pose: initial position of the quadcopter in (x,y,z) dimensions and the Euler angles
-            init_velocities: initial velocity of the quadcopter in (x,y,z) dimensions
-            init_angle_velocities: initial radians/second for each of the three Euler angles
-            runtime: time limit for each episode
-            target_pos: target/goal (x,y,z) position for the agent
         """
         # Simulation
-        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
-        self.action_repeat = 3
+        self.runtime = 5.
+        self.init()
+        self.action_repeat = 1
 
-        self.state_size = self.action_repeat * 6
+        self.state_size = self.action_repeat * 9
         self.action_low = 0
         self.action_high = 900
         self.action_size = 4
 
         # Goal
-        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.]) 
+        # self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 10.])
 
     def get_reward(self):
         """Uses current pose of sim to return reward."""
-        reward = 1.-.3*(abs(self.sim.pose[:3] - self.target_pos)).sum()
+        dist = np.linalg.norm(np.array(self.sim.pose[:3])-np.array(self.target_pos))
+        reward = 1.-.2*(dist).sum()
         return reward
 
     def step(self, rotor_speeds):
@@ -37,13 +34,20 @@ class Task():
         pose_all = []
         for _ in range(self.action_repeat):
             done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities
-            reward += self.get_reward() 
-            pose_all.append(self.sim.pose)
+            reward += self.get_reward()
+            pose_all.append(np.concatenate([self.sim.pose, self.target_pos]))
         next_state = np.concatenate(pose_all)
         return next_state, reward, done
 
+    def init(self):
+        self.init_pose = np.array(np.concatenate(
+          [[float(random.randint(0,10)) for x in range(3)], [0., 0., 0.]]))
+        self.target_pos = np.array([ float(random.randint(0,10)) for x in range(3)])
+        self.sim = PhysicsSim(self.init_pose, None, None, self.runtime)
+
     def reset(self):
         """Reset the sim to start a new episode."""
+        self.init()
         self.sim.reset()
-        state = np.concatenate([self.sim.pose] * self.action_repeat) 
+        state = np.concatenate([np.concatenate([self.sim.pose, self.target_pos])] * self.action_repeat)
         return state
